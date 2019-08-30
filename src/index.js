e706b053aac1c232b2786607254f5219bc4c677b
Kim Jeonghwan <ej88ej@gmail.com>
2019-08-30 20:48:00 +0900

    로그인 구현

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const app = express()
+
+const users = [{id: 1, username: 'alice', password: 'qwer1234'}]
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  const user = users.find(user => user.id === id)
+  done(null, user)
+})
+
+passport.use(new LocalStrategy((username, password, done) => {
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return done(null, false, {
+      message: `username '${username}' not found.`
+    })
+  }
+
+  if (user.password !== password) {
+    return done(null, false, {
+      message: 'password incorrect'
+    })
+  }
+
+  done(null, user)
+}))
+
 
 app.set('view engine', 'pug')
 app.set('views', path.resolve(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use(session({
+  secret: 'cats',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
   res.render('home', {title: '홈'})
@@ -16,8 +58,17 @@ app.get('/login', (req, res) => {
   res.render('login', {title: '로그인'})
 })
 
-app.post('/login', (req, res) => {
-  res.send('login api')
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err)
+
+    if (!user) return res.json(info)
+
+    req.logIn(user, (err) => {
+      if (err) return next(err)
+      res.json({ user })
+    })
+  })(req, res, next)
 })
 
 app.get('/logout', (req, res) => {
